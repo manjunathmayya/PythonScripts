00c3a707cf567a993468a90d2f8ae942cfd6efb9
manjunathmayya <41788269+manjunathmayya@users.noreply.github.com>
2019-02-07 18:06:57 +0530

    Parse cricket score and notify if a wicket falls or on boundary
    Enter the URL in the text box and press start button.
    ex: 'http://www.espncricinfo.com/series/8726/game/1157486/eastern-province-vs-boland-pool-a-csa-3-day-provincial-cup-2018-19'

---
+++ CricinfoScoreParser.py
@@ -0,0 +1,121 @@
+#import webbrowser
+import requests
+from lxml.html import fromstring
+#import tkinter as tk
+#from tkinter import ttk
+import wx
+#from tkinter import *
+import winsound 
+#webbrowser.open('https://www.google.co.in/maps/place/united states of america')
+
+
+def GetScore(link):
+    http_proxy  = "http://194.138.0.25:9400"
+    https_proxy = "https://194.138.0.25:9400"
+    
+    
+    proxyDict = { 
+                  "http"  : http_proxy, 
+                  "https" : https_proxy,               
+                }
+    
+    s = requests.Session()
+    #s.proxies = {"http://proxyconf-aae-in.siemens.co.in:81/proxyin.pac/"}
+    s.proxies = proxyDict 
+    
+    if link == '':
+        link = 'http://www.espncricinfo.com/series/8044/game/1152562/hobart-hurricanes-vs-melbourne-renegades-52nd-match-big-bash-league-2018-19' 
+        
+    r = s.get(link)
+    tree = fromstring(r.content)
+    score = tree.findtext('.//title')
+    return score
+
+
+#win = tk.Tk()
+#matchScore = GetScore()
+#win.title('Score')    
+#ttk.Label(win, text = matchScore).grid(column =0, row = 0)
+#win.mainloop()
+
+
+
+
+ 
+class MyForm(wx.Frame):
+ 
+    def __init__(self):
+        wx.Frame.__init__(self, None, wx.ID_ANY, "Scorer",size=(750,70))
+ 
+        # Add a panel so it looks the correct on all platforms
+        panel = wx.Panel(self, wx.ID_ANY)
+ 
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.update, self.timer)
+ 
+        self.toggleBtn = wx.Button(panel, wx.ID_ANY, "Start")
+        self.toggleBtn.Bind(wx.EVT_BUTTON, self.onToggle)
+        
+        self.linkText = wx.TextCtrl(panel)
+        
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        
+        panel.SetSizer(hbox)
+        
+        hbox.Add(self.linkText, 4)
+        hbox.Add((-1, 10))
+ 
+        hbox.Add(self.toggleBtn)
+        hbox.Add((-1, 10))
+        
+        self.link=''
+        self.PreviousRuns = 0;
+        self.PreviousWickets = 0;
+        
+
+    def onToggle(self, event):
+        btnLabel = self.toggleBtn.GetLabel()
+        self.link = self.linkText.GetValue()
+        
+        if btnLabel == "Start":
+            print ("starting timer...")
+            self.timer.Start(5000)
+            self.toggleBtn.SetLabel("Stop")
+        else:
+            print ("timer stopped!")
+            self.timer.Stop()
+            self.toggleBtn.SetLabel("Start")
+ 
+    def update(self, event):
+        self.score = GetScore(self.link)        
+        self.SetTitle(self.score)
+        summary = self.score #Example score: 'PAK-W 70/8 (29.5 ov, batsman 0*, batsman2 2*, bowler 3/17)'
+        summarySplit = summary.split(' ')
+        score = summarySplit[1] 
+        Runs = score.split('/')[0]
+        Wickets = score.split('/')[1]
+        
+        if (int(Runs)-int(self.PreviousRuns)) >= 4:
+            self.Iconize(False)
+        
+        if int(Wickets) - int(self.PreviousWickets) > 0:            
+            self.Iconize(False)        
+            winsound.Beep(2500, 500)
+        
+        self.PreviousRuns = Runs;
+        self.PreviousWickets = Wickets;        
+        
+def main():
+   app = wx.App()
+   frame = MyForm()
+   frame.Show()
+   app.MainLoop()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
