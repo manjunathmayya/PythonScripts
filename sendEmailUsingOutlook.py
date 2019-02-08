898900bffda2b086292b4d162774a6e82df5114a
manjunathmayya <41788269+manjunathmayya@users.noreply.github.com>
2019-02-08 18:19:49 +0530

    Send email using outlook

---
+++ sendEmailUsingOutlook.py
@@ -0,0 +1,39 @@
+import win32com.client as win32
+import argparse
+
+def SendEmail(emailAddress, subject, message):
+    outlook = win32.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+    mail.To = emailAddress
+    mail.Subject = subject
+    mail.Body = message
+    mail.HTMLBody = '<h2>' + message +'</h2>' #this field is optional
+    # To attach a file to the email (optional):
+    #attachment  = "Path to the attachment"
+    #mail.Attachments.Add(attachment)    
+    mail.Send()
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--emailAddress',
+    '-e',
+    default='manjunath.k@siemens-healthineers.com',
+    help='Enter email address of the recepient')
+parser.add_argument(
+    '--subject',
+    '-s',
+    default='Subject',
+    help='Enter subject for the email')
+parser.add_argument(
+    '--message',
+    '-m',    
+    default='message',
+    help='Enter message for the email')
+
+args = parser.parse_args()
+
+SendEmail(args.emailAddress, args.subject, args.message)
+
+
