4574be825bc9d93e93c44b1757db8eab50992685
manjunathmayya <41788269+manjunathmayya@users.noreply.github.com>
2019-03-20 21:49:12 +0530

    Update and rename config.py to StatusUpdate/config.py

---
+++ StatusUpdate/config.py
@@ -0,0 +1,7 @@
+filename = 'D:\analysis.xlsx'
+file_to_attach = 'D:\\Status.xlsx'
+sheetname	= 'StatusToSend'
+to	= 'manjunathmayya@gmail.com'
+cc	= 'someone@gmail.com'
+subject_to_find	= 'Refactoring of Web Module'
+content = "Hi, <br /> Please find the status of the task"
--- config.py
+++
@@ -1,7 +0,0 @@
-filename = 'D:\\CCstorages\\views\\copy_of_analysis.xlsx'
-file_to_attach = 'D:\\CCstorages\\views\\Status.xlsx'
-sheetname	= 'StatusToSend'
-to	= 'manjunath.k@siemens-healthineers.com'
-cc	= ''
-subject_to_find	= 'Regarding SYC VE21 Trace and setError changes'
-content = "Hi, <br /> Please find the status of the task"
